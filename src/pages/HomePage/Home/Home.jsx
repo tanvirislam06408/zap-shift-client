7da05168dacab692d0e7afd9516612ce38137052
tanvir <mstanvirislam05@gmail.com>
2026-04-16 19:11:19 +0600

    feat: implement Marchent component and integrate into Home; update Support with AOS animation

--- src/pages/HomePage/Home/Home.jsx
+++ src/pages/HomePage/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 import BrandMarquee from '../BrandMarquee/BrandMarquee';
 import Support from '../Support/Support';
+import Marchent from '../Marchent/Marchent';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Services/>
            <BrandMarquee/>
            <Support/>
+           <Marchent/>
         </div>
     );
 };
